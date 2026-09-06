8264b49eedb6699676166f27856a3c00f5461eb6
Hussein Jasin <Husseinjasin@gmail.com>
2026-09-06 02:17:41 +0100

    Connect Flask to Redis

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask 
+from redis import Redis
 
 app = Flask(__name__)
 
+redis_client =  Redis (host="redis", port=6379)
+
 @app.route("/")
 def home ():
+    redis_client.incr("visits")
     return "Hello from my Docker application, this is edition 2 !"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
